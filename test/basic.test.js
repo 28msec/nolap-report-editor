b8209c5e23032239deb5890b4c77041cb004db41
Dennis Knochenwefel <d@28.io>
2014-05-19 17:19:22 +0000

    more warning fixes

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,15 +1,15 @@
 describe('Basic Test', function () {
     it('Create a New Report', function () {
         var report = new Report({
-            '_id' : 'ReportName', 
-            'Archive' : null, 
-            'Label' : 'Report Label', 
+            '_id' : 'ReportName',
+            'Archive' : null,
+            'Label' : 'Report Label',
             'Description': 'Default report',
-            'Role' : 'http://my.com/report-role', 
-            'Networks' : [], 
-            'Hypercubes' : {}, 
+            'Role' : 'http://my.com/report-role',
+            'Networks' : [],
+            'Hypercubes' : {},
             'Rules' : []
-        });     
+        });
         var model = report.getModel();
         expect(model._id).toBeDefined();
         expect(model._id).toEqual('ReportName');
