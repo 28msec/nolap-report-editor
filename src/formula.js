660adf041c2a31b31ee2112e2a4dd43e9135f650
Dennis Knochenwefel <d@28.io>
2014-05-27 13:22:47 +0000

    formula deferred compilation

--- src/formula.js
+++ src/formula.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('formulaEditor',['excelParser'])
-.factory('Formula', [ '$log', 'ExcelParser', function($log, ExcelParser){
+angular.module('formulaEditor',['excelParser', 'formulaParser'])
+.factory('Formula', ['$q', '$log', 'ExcelParser', 'FormulaParser', function($q, $log, ExcelParser, FormulaParser){
 
     var ensureParameter = function(paramValue, paramName, paramType, functionName, regex, regexErrorMessage) {
         if(paramValue === null || paramValue === undefined) {
@@ -18,6 +18,7 @@ angular.module('formulaEditor',['excelParser'])
     //Constructor
     var Formula = function(type){
         ensureParameter(type, 'type', 'string', 'Formula (Const.)');
+        this.parser = null;
         this.model = {
             'Id': '',
             'Type': type,
@@ -40,7 +41,14 @@ angular.module('formulaEditor',['excelParser'])
 
     Formula.prototype.getParser = function() {
         if(this.parser === undefined || this.parser === null ) {
-            this.parser = new ExcelParser();
+            if(this.model.Type === 'xbrl28:arithmetic') {
+                this.parser = new FormulaParser();
+                this.parserType = 'FormulaParser';
+            }
+            if(this.model.Type === 'xbrl28:excel') {
+                this.parser = new ExcelParser();
+                this.parserType = 'ExcelParser';
+            }
         }
         return this.parser;
     };
@@ -50,55 +58,104 @@ angular.module('formulaEditor',['excelParser'])
     };
 
 
-    Formula.prototype.compile = function() {
+    Formula.prototype.compileDeferred = function() {
+        var deferred = $q.defer();
+
+        $log.log('starting compilation');
         for (var i = 0; i < this.model.Formulae.length; ++i) {
             this.compilePrereq(i);
             this.compileBody(i);
         }
+
+        return deferred.promise;
     };
 
-    Formula.prototype.compileBody = function(index) {
-        ensureParameter(index, 'index', 'number', 'compileBody');
+    Formula.prototype.compile = function() {
+        this.compileDeferred().then(
+            function(){
+                $log.log('compilation finished');
+            }
+        );
+    };
+
+    Formula.prototype.compileBodyDeferred = function(index) {
+        ensureParameter(index, 'index', 'number', 'compileBodyDeferred');
+        var deferred = $q.defer();
         var parser = this.getParser();
         var altComp = this.model.Formulae[index];
         var body;
         try{
             body = parser.parse(altComp.BodySrc);
-            altComp.Body = body;
-            if(altComp.BodyErr !== undefined){
-                delete altComp.BodyErr;
-            }
-            $log.log("Body ok");
+            deferred.resolve(body);
         }
         catch(e) {
-            var errMsg = e.name + ' Error: (' + e.line + ',' + e.column + ') ' + e.message;
-            altComp.BodyErr = errMsg;
-            altComp.Body = {};
-            $log.log(errMsg);
+            var errMsg = this.parserType + ' ' + e.name + ' Error: (' + e.line + ',' + e.column + ') ' + e.message;
+            deferred.reject(errMsg);
         }
-        this.model.Formulae[index] = altComp;
+        return deferred.promise;
     };
 
-    Formula.prototype.compilePrereq = function(index) {
-        ensureParameter(index, 'index', 'number', 'compilePrereq');
+    Formula.prototype.compileBody = function(index) {
+        ensureParameter(index, 'index', 'number', 'compileBody');
+        var that = this;
+        this.compileBodyDeferred(index).then(
+            function(body){
+                var altComp = that.model.Formulae[index];
+                altComp.Body = body;
+                if(altComp.BodyErr !== undefined){
+                    delete altComp.BodyErr;
+                }
+                $log.log(that.parserType + ' Body ok');
+                that.model.Formulae[index] = altComp;
+            },
+            function(errMsg){
+                var altComp = that.model.Formulae[index];
+                altComp.BodyErr = errMsg;
+                altComp.Body = {};
+                $log.log(errMsg);
+                that.model.Formulae[index] = altComp;
+            }
+        );
+    };
+
+    Formula.prototype.compilePrereqDeferred = function(index) {
+        ensureParameter(index, 'index', 'number', 'compilePrereqDeferred');
+        var deferred = $q.defer();
         var parser = this.getParser();
         var altComp = this.model.Formulae[index];
         var prereq;
         try{
             prereq = parser.parse(altComp.PrereqSrc);
-            altComp.Prereq = prereq;
-            if(altComp.PrereqErr !== undefined){
-                delete altComp.PrereqErr;
-            }
-            $log.log("Prereq ok");
+            deferred.resolve(prereq);
         }
         catch(e) {
-            var errMsg = e.name + ' Error: (' + e.line + ',' + e.column + ') ' + e.message;
-            altComp.PrereqErr = errMsg;
-            altComp.Prereq = {};
-            $log.log(errMsg);
+            var errMsg = this.parserType + ' ' + e.name + ' Error: (' + e.line + ',' + e.column + ') ' + e.message;
+            deferred.reject(errMsg);
         }
-        this.model.Formulae[index] = altComp;
+        return deferred.promise;
+    };
+
+    Formula.prototype.compilePrereq = function(index) {
+        ensureParameter(index, 'index', 'number', 'compilePrereq');
+        var that = this;
+        this.compilePrereqDeferred(index).then(
+            function(prereq){
+                var altComp = that.model.Formulae[index];
+                altComp.Prereq = prereq;
+                if(altComp.PrereqErr !== undefined){
+                    delete altComp.PrereqErr;
+                }
+                $log.log(that.parserType + ' Prereq ok');
+                that.model.Formulae[index] = altComp;
+            },
+            function(errMsg){
+                var altComp = that.model.Formulae[index];
+                altComp.PrereqErr = errMsg;
+                altComp.Prereq = {};
+                $log.log(errMsg);
+                that.model.Formulae[index] = altComp;
+            }
+        );
     };
 
     Formula.prototype.getModel = function () {
@@ -108,6 +165,8 @@ angular.module('formulaEditor',['excelParser'])
     Formula.prototype.setModel = function (model) {
         ensureParameter(model, 'model', 'object', 'setModel');
         this.model = model;
+        this.parser = null;
+        this.compile();
     };
 
     Formula.prototype.Id = function() {
@@ -115,6 +174,4 @@ angular.module('formulaEditor',['excelParser'])
     };
     return Formula;
 }]);
-
-
 
