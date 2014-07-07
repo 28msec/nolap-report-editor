2e77d0083f44c050106c27aec148ff1b10f67b00
William Candillon <wcandillon@gmail.com>
2014-07-07 18:10:19 +0200

    Bootstrap treeview

--- app/report/report.js
+++ app/report/report.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('report-editor')
-.controller('ReportCtrl', function($scope, $state, report){
-    $scope.report = report[0];
+.controller('ReportCtrl', function($scope, $state, report, Report){
+    $scope.report = new Report(report[0]);
 
     if($state.current.name === 'report') {
         $state.go('report.taxonomy');
