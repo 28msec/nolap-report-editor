3958924ef4c5bad3e2676ebb546c4e3354b51035
William Candillon <wcandillon@gmail.com>
2014-07-07 13:25:07 +0200

    Add 404 handling

--- app/app.js
+++ app/app.js
@@ -54,10 +54,7 @@ angular.module('report-editor', [
     $stateProvider
     .state('500', {
         templateUrl: '/500.html'
-    })
-    .state('404', {
-        templateUrl: '/404.html'
-    })
+    });
 })
 
 .run(function($rootScope, ngProgressLite, $state, $location, API, Session) {
