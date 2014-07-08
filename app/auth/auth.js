26b5f9a36fea2800672f0cdc0df10a9915900250
Dennis Knochenwefel <d@28.io>
2014-07-08 15:08:17 +0200

    fixed browser autofill issue

--- app/auth/auth.js
+++ app/auth/auth.js
@@ -1,40 +1,41 @@
 'use strict';
 
 angular.module('report-editor')
-    .controller('AuthCtrl', function($scope, $stateParams, $location, $http, $window, Session, REGISTRATION_URL) {
+    .controller('AuthCtrl', function($scope, $stateParams, $location, $http, $timeout, Session, REGISTRATION_URL) {
+
         $scope.returnPage = $stateParams.returnPage;
         $scope.registrationURL = REGISTRATION_URL;
         $scope.loginAttempted = false;
 
         $scope.login = function(){
-            $scope.loginAttempted = true;
-            $scope.loginForm.loginPassword.$setValidity('unauthorized', true);
-            if(!$scope.loginForm.$invalid) {
-                $scope.loading = true;
-                Session
-                .login($scope.loginEmail, $scope.loginPassword)
-                .then(function() {
-                    $location.url(decodeURIComponent($scope.returnPage || '/')).replace();
-                })
-                .catch(function() {
-                    $scope.loginForm.loginPassword.$setValidity('unauthorized', false);
-                    $scope.loading = false;
-                });
-            }
+            $scope.$broadcast("autofill:update");
+
+            $timeout(function(){
+                $scope.loginAttempted = true;
+                $scope.loginForm.loginPassword.$setValidity('unauthorized', true);
+                if(!$scope.loginForm.$invalid) {
+                    $scope.loading = true;
+                    Session
+                    .login($scope.loginEmail, $scope.loginPassword)
+                    .then(function() {
+                        $location.url(decodeURIComponent($scope.returnPage || '/')).replace();
+                    })
+                    .catch(function() {
+                        $scope.loginForm.loginPassword.$setValidity('unauthorized', false);
+                        $scope.loading = false;
+                    });
+                }
+            }, 100);
         };
     })
-    .directive('checkForPrefilled', function($timeout){
+    .directive('autofillCheck', function(){
         return {
-            link: function($scope, element){
-                $timeout(function () {
-                    // fix for: https://github.com/28msec/nolap-report-editor/issues/19
-                    var inputField = element[0];
-                    if(inputField !== undefined && inputField !== null && inputField.value !== '') {
-                        console.log(inputField.name);
-                        // @TODO
-                        //inputField.trigger('input');
-                    }
-                }, 500);
+            require: 'ngModel',
+            link: function($scope, element, attrs, ngModel){
+                // fix for: https://github.com/28msec/nolap-report-editor/issues/19
+                $scope.$on("autofill:update", function() {
+                    ngModel.$setViewValue(element.val());
+                });
             }
         };
     });
