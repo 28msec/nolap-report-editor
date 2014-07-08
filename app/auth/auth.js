3ea809dc97be142515f486018152b7edc95c636b
Dennis Knochenwefel <d@28.io>
2014-07-08 19:59:04 +0200

    fixed jshint warnings

--- app/auth/auth.js
+++ app/auth/auth.js
@@ -8,7 +8,7 @@ angular.module('report-editor')
         $scope.loginAttempted = false;
 
         $scope.login = function(){
-            $scope.$broadcast("autofill:update");
+            $scope.$broadcast('autofill:update');
 
             $timeout(function(){
                 $scope.loginAttempted = true;
@@ -33,7 +33,7 @@ angular.module('report-editor')
             require: 'ngModel',
             link: function($scope, element, attrs, ngModel){
                 // fix for: https://github.com/28msec/nolap-report-editor/issues/19
-                $scope.$on("autofill:update", function() {
+                $scope.$on('autofill:update', function() {
                     ngModel.$setViewValue(element.val());
                 });
             }
