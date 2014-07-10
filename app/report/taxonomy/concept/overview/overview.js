d384c7b0b6af4dc5fb1ed395df94038ea711238d
William Candillon <wcandillon@gmail.com>
2014-07-10 17:17:28 +0200

    Add create and delete concept

--- app/report/taxonomy/concept/overview/overview.js
+++ app/report/taxonomy/concept/overview/overview.js
@@ -2,6 +2,15 @@
 
 angular
 .module('report-editor')
-.controller('ConceptOverviewCtrl', function($scope){
-    console.log($scope.concept); 
+.controller('ConceptOverviewCtrl', function($scope, $state){
+    console.log($scope.concept);
+    
+    $scope.deleteConcept = function(){
+        try {
+            $scope.report.removeConcept($scope.concept.Name);
+            $state.go('report.taxonomy.concepts');
+        } catch(e) {
+            console.error(e);
+        }
+    };
 });
