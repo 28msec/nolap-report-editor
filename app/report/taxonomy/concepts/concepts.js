8f85a8a4df482423f1bbe4d9585db4d8fcf49123
William Candillon <wcandillon@gmail.com>
2014-07-10 14:15:40 +0200

    Improve concept typeahead

--- app/report/taxonomy/concepts/concepts.js
+++ app/report/taxonomy/concepts/concepts.js
@@ -2,7 +2,26 @@
 
 angular
 .module('report-editor')
-.controller('ConceptsCtrl', function($scope){
+.controller('ConceptsCtrl', function($scope, $state){
     $scope.concepts = $scope.report.listConcepts();
+    $scope.getResults = function($viewValue){
+        var results = [];
+        $scope.concepts.forEach(function(concept) {
+            if(concept.Name.indexOf($viewValue) !== -1) {
+                results.push(concept);
+            }
+        });
+        results.push({ Name: 'Create ' + $viewValue + ' concept.', newConcept: true });
+        return results;
+    };
+    
+    $scope.onSelect = function($item, $model, $label){
+        if($item.newConcept === true) {
+            
+        } else {
+            $state.go('report.taxonomy.concept', { conceptId: $item.Name });
+        }
+    };
+    
     console.log($scope.concepts[0]);
 });
