94eb970859e1f3c4e1ce4ee707f0b3d7032d5d89
William Candillon <wcandillon@gmail.com>
2014-07-10 17:21:59 +0200

    Improve persistance algorithm

--- app/report/report.js
+++ app/report/report.js
@@ -4,11 +4,13 @@ angular.module('report-editor')
 .controller('ReportCtrl', function($q, $rootScope, $scope, Session, API, Report, report){
     $scope.report = report;
 
+    var lastKnowSavedModel = $scope.report.model;
+    var lastKnowSavedModelAsString = JSON.stringify(lastKnowSavedModel);
     var saveCanceler;
 
     $scope.$watch('report.model', function(newVal, oldVal){
         //TODO: JSON stringify is too slow
-        if(oldVal === undefined || JSON.stringify(newVal) === JSON.stringify(oldVal)) {
+        if(oldVal === undefined || JSON.stringify(newVal) === lastKnowSavedModelAsString) {
             return;
         }
         $rootScope.$emit('apiStatus', { message: 'Saving Report...', code: 'warning' });
@@ -21,14 +23,16 @@ angular.module('report-editor')
             token: Session.getToken(),
             $timeout: saveCanceler.promise
         })
-        .then(function(response){  
+        .then(function(response){
+            lastKnowSavedModel = newVal;
+            lastKnowSavedModelAsString = JSON.stringify(lastKnowSavedModel);
             $rootScope.$emit('apiStatus', { message: 'Report saved', code: 'success', expires: 500 });
         })
         .catch(function(error){
             //error.statuys === 0 means two things: either we don't have internet of the http call has been canceled out. Either way it's not handled here
             if(error.status !== 0) {
                 $rootScope.$emit('apiStatus', { message: 'Error while saving', code: 'error', expires: 4000 });
-                $scope.report = new Report(oldVal);
+                $scope.report = new Report(lastKnowSavedModelAsString);
             }
         });
     }, true);
