e63681f2497579ef8e3f2e8be7ecbe9ca3043c9d
William Candillon <wcandillon@gmail.com>
2014-07-15 16:06:53 -0700

    Minor improvements

--- app/report/taxonomy/taxonomy.js
+++ app/report/taxonomy/taxonomy.js
@@ -1,29 +1,6 @@
 'use strict';
 angular
 .module('report-editor')
-.directive('sglclick', function($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attr) {
-            var fn = $parse(attr['sglclick']);
-            var delay = 300, clicks = 0, timer = null;
-            element.on('click', function (event) {
-                clicks++;  //count clicks
-                if(clicks === 1) {
-                    timer = setTimeout(function() {
-                        scope.$apply(function () {
-                            fn(scope, { $event: event });
-                        });
-                        clicks = 0;             //after action performed, reset counter
-                    }, delay);
-                } else {
-                    clearTimeout(timer);    //prevent single-click action
-                    clicks = 0;             //after action performed, reset counter
-                }
-            });
-        }
-    };
-})
 .controller('TaxonomyCtrl', function($scope, $state){
 
     $scope.treeOptions = {
