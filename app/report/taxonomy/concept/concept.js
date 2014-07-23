5fe35f26b94867cf9335ffa27621be5b1a2844e9
Dennis Knochenwefel <d@28.io>
2014-07-23 22:27:36 +0200

    disable validations for validation concepts

--- app/report/taxonomy/concept/concept.js
+++ app/report/taxonomy/concept/concept.js
@@ -5,6 +5,12 @@ angular.module('report-editor')
 
     $scope.concept = $scope.report.getConcept($stateParams.conceptId);
     $scope.selectConcept($scope.concept);
+    $scope.computingRules = $scope.report.computableByRules($scope.concept.Name);
+    if($scope.computingRules[0] !== undefined && $scope.computingRules[0].Type === 'xbrl28:validation'){
+        $scope.isValidationConcept = true;
+    } else {
+        $scope.isValidationConcept = false;
+    }
 
     $scope.$watch('concept.IsAbstract', function(newVal, oldVal){
         if(newVal !== oldVal) {
