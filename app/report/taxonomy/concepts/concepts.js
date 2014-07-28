2a3fc24c20edd93e89b767a359815f641386cdd1
William Candillon <wcandillon@gmail.com>
2014-07-28 16:57:12 -0700

    fixes #108

--- app/report/taxonomy/concepts/concepts.js
+++ app/report/taxonomy/concepts/concepts.js
@@ -5,6 +5,10 @@ angular
 .controller('ConceptsCtrl', function($scope, $state){
     $scope.concepts = $scope.report.listConcepts();
     $scope.unselectElement();
+    
+    var splitCamelCase = function(value){
+        return value.replace(/([a-z0-9])([A-Z])/g, '$1 $2').replace(/^./, function(str){ return str.toUpperCase(); });
+    };
 
     $scope.getResults = function($viewValue){
         var results = [];
@@ -19,7 +23,7 @@ angular
     
     $scope.onSelect = function($item){
         if($item.newConcept === true) {
-            $scope.report.addConcept($item.viewValue, 'Concept Label', false);
+            $scope.report.addConcept($item.viewValue, splitCamelCase($item.viewValue), false);
         }
         $state.go('report.taxonomy.concept.overview', { conceptId: $item.newConcept ? $item.viewValue : $item.Name });
     };
