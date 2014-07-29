6835eef380cbbf3d0dd670c8265807cef5a10b09
William Candillon <wcandillon@gmail.com>
2014-07-29 15:34:26 -0700

    Add isolation in the unit test

--- app/report/taxonomy/concept/overview/overview-test.js
+++ app/report/taxonomy/concept/overview/overview-test.js
@@ -2,27 +2,14 @@ describe('Check Concept updating ', function () {
     'use strict';
 
     var scope, rootScope, createController;
-    var assetsElement;
 
     beforeEach(inject(function($rootScope, $controller, Report) {
         rootScope = $rootScope;
         scope = $rootScope.$new();
 
-        // create report
         scope.report = new Report('slfkjalrfjweoila', 'Hello World', 'Description', 'Role', 'd@28.io');
-
-        //create concepts
         scope.report.addConcept('hw:hierarchy', 'Hierarchy', true);
-        scope.report.addConcept('hw:Assets', 'Revenues', false);
-
-        // add concepts to presentation tree
-        var hierarchyElement = scope.report.createNewElement('hw:hierarchy');
-        assetsElement = scope.report.createNewElement('hw:Assets');
-        scope.report.addElement('Presentation', null, hierarchyElement, 0);
-        scope.report.addElement('Presentation', null, assetsElement, 0);
-
-        // put concept into scope
-        scope.concept = scope.report.getConcept('hw:Assets');
+        scope.concept = scope.report.getConcept('hw:hierarchy');
 
         createController = function() {
             return $controller('ConceptOverviewCtrl', {
@@ -32,21 +19,17 @@ describe('Check Concept updating ', function () {
     }));
     
 
-    it('changes the label in the presentation tree elements when changing a concept label.', function(){
+    it('Basic checks on the Overview controller.', function(){
+        //Controller instanciate smoothly
         createController();
-        expect(scope.report.getNetwork('Presentation').Trees).toBeDefined();
-        expect(scope.report.findInTree('Presentation','hw:Assets').length).toEqual(1);
-        expect(assetsElement.Label).toEqual('Revenues');
+        //The concept display in the form of the page is a copy
         expect(scope.concept).toEqual(scope.conceptCopy);
+        expect(scope.concept === scope.conceptCopy).toBe(false);
 
+        //Update Concept runs smoothly
         scope.conceptCopy.Label = 'Assets';
         expect(scope.concept).not.toEqual(scope.conceptCopy);
-
         scope.updateConcept();
-
-        expect(scope.report.findInTree('Presentation','hw:Assets').length).toEqual(1);
-        expect(scope.concept).toEqual(scope.conceptCopy);
-        expect(assetsElement.Label).toEqual('Assets');
     });
 
 });
