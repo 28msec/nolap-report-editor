60038f27f9ec49762ac60326f7dfe4b84013df95
William Candillon <wcandillon@gmail.com>
2014-08-12 14:59:48 +0200

    Update script

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function (config) {
     config.set({
         basePath: './',
         frameworks: ['jasmine'],
-        browsers: ['Firefox', 'PhantomJS'],
+        browsers: ['PhantomJS'],
         files: [
         ],
         captureTimeout: 60000,
