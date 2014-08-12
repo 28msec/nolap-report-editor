00922d86c5b93cf3104989ff1a57fa04a289a5f4
William Candillon <wcandillon@gmail.com>
2014-08-12 14:45:16 +0200

    Update script

--- tests/e2e/config/protractor-saucelabs-conf.js
+++ tests/e2e/config/protractor-saucelabs-conf.js
@@ -6,28 +6,31 @@ config.sauceUser = process.env.SAUCE_USERNAME;
 config.sauceKey = process.env.SAUCE_ACCESS_KEY;
 
 config.multiCapabilities = [
+//{
+//    'browserName': 'chrome',
+//    'platform': 'OS X 10.9',
+//    'name': 'NoLAP Report Editor',
+//    'tunnel-identifier': process.env.WERCKER_STEP_ID,
+//    'build': process.env.WERCKER_GIT_COMMIT,
+//    'version': '34'
+//}
+//, 
 {
-    'browserName': 'chrome',
-    'platform': 'OS X 10.9',
-    'name': 'NoLAP Report Editor',
-    'tunnel-identifier': process.env.WERCKER_STEP_ID,
-    'build': process.env.WERCKER_GIT_COMMIT,
-    'version': '34'
-}, {
     'browserName': 'firefox',
     'name': 'NoLAP Report Editor',
     'tunnel-identifier': process.env.WERCKER_STEP_ID,
     'build': process.env.WERCKER_GIT_COMMIT,
     'version': '31'
-},
-{
-    browserName: 'safari',
-    'platform': 'OS X 10.9',
-    'name': 'NoLAP Report Editor',
-    'tunnel-identifier': process.env.WERCKER_STEP_ID,
-    'build': process.env.WERCKER_GIT_COMMIT,
-    'version': '7'
 }
+//,
+//{
+//    browserName: 'safari',
+//    'platform': 'OS X 10.9',
+//    'name': 'NoLAP Report Editor',
+//    'tunnel-identifier': process.env.WERCKER_STEP_ID,
+//    'build': process.env.WERCKER_GIT_COMMIT,
+//    'version': '7'
+//}
 ];
 
 config.allScriptsTimeout = 30000;
