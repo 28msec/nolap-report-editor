7257da450d10712632163156b501606a598d2e11
Dennis Knochenwefel <d@28.io>
2014-09-12 12:37:41 +0200

    implement full details modal for spreadsheet and facttabel view

--- app/report/spreadsheet/spreadsheet.js
+++ app/report/spreadsheet/spreadsheet.js
@@ -39,7 +39,7 @@ angular.module('report-editor')
 
     $scope.onDataCellClick = function(data){
         $modal.open({
-            template: '<fact-details-modal fact="fact" />',
+            templateUrl: '/modules/ui/fact-details-modal.html',
             controller: 'FactDetailCtrl',
             size: 'lg',
             resolve: {
